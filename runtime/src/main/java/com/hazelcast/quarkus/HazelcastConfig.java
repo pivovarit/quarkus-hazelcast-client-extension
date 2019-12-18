c2f0966e946e42b69cc83552f17765e7ca153d0c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-18 11:35:00 +0100

    Add HazelcastConfig

--- runtime/src/main/java/com/hazelcast/quarkus/HazelcastConfig.java
+++ runtime/src/main/java/com/hazelcast/quarkus/HazelcastConfig.java
@@ -1,4 +1,12 @@
 package com.hazelcast.quarkus;
 
-class HazelcastConfig {
+import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+@ConfigRoot(name = "hazelcast", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+public final class HazelcastConfig {
+
+    @ConfigItem
+    public String clusterAddress; // quarkus.hazelcast.cluster-address=192.168.1.1
 }
