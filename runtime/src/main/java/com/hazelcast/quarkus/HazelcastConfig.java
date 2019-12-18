c6fc43f48c2998bf25c2099f1b95b184f5f9ade0
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-18 11:40:18 +0100

    Initial Project Structure (#1)

---
+++ runtime/src/main/java/com/hazelcast/quarkus/HazelcastConfig.java
@@ -0,0 +1,15 @@
+package com.hazelcast.quarkus;
+
+import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+@ConfigRoot(name = "hazelcast", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+public final class HazelcastConfig {
+
+    /**
+     * Hazelcast cluster address
+     */
+    @ConfigItem
+    public String clusterAddress; // quarkus.hazelcast.cluster-address=192.168.1.1
+}
