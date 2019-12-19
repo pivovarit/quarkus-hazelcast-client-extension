8cefc182371a9db4293437b6c95d8234306113a8
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-19 09:34:57 +0100

    Initial

--- runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
+++ runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
@@ -9,11 +9,17 @@ import javax.enterprise.inject.Produces;
 
 @ApplicationScoped
 public class HazelcastClientProducer {
+    private HazelcastClientConfig hazelcastClientConfig;
 
     @Produces
     public HazelcastInstance instance() {
         ClientConfig clientConfig = new ClientConfig();
-        clientConfig.getNetworkConfig().addAddress("member1", "member2");
+        System.out.println("hz_config: " + hazelcastClientConfig.clusterAddress);
+        clientConfig.getNetworkConfig().addAddress(hazelcastClientConfig.clusterAddress.split(","));
         return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+
+    public void setHazelcastClientConfig(HazelcastClientConfig hazelcastClientConfig) {
+        this.hazelcastClientConfig = hazelcastClientConfig;
     }
 }
