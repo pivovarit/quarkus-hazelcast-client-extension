f7cb6bdb853c4470e72855dbc267a44f5f22c543
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-29 17:13:24 +0200

    Register XML parsing classes

--- deployment/src/main/java/io/quarkus/hazelcast/client/deployment/HazelcastClientProcessor.java
+++ deployment/src/main/java/io/quarkus/hazelcast/client/deployment/HazelcastClientProcessor.java
@@ -37,6 +37,18 @@ class HazelcastClientProcessor {
     }
 
     @BuildStep
+    void registerXmlParsingClasses(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        reflectiveClass.produce(new ReflectiveClassBuildItem(false, false,
+          "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl",
+          "com.sun.org.apache.xerces.internal.jaxp.datatype.DatatypeFactoryImpl",
+          "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl",
+          "com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl",
+          "com.sun.xml.bind.v2.ContextFactory",
+          "com.sun.xml.internal.bind.v2.ContextFactory",
+          "com.sun.xml.internal.stream.XMLInputFactoryImpl"));
+    }
+
+    @BuildStep
     void registerResources(BuildProducer<NativeImageResourceBuildItem> resources) {
         resources.produce(new NativeImageResourceBuildItem("hazelcast.yml", "hazelcast.xml"));
     }
