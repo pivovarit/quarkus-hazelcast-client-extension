e3b38e49ae2340c7e75a712513015fcc6b7d8686
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-03 17:36:16 +0200

    Wire-in SocketInterceptor in test app (#63)

---
+++ test/quarkus-test-app-xml/src/main/java/guides/hazelcast/quarkus/HelloSocketInterceptor.java
@@ -0,0 +1,19 @@
+package guides.hazelcast.quarkus;
+
+import com.hazelcast.nio.SocketInterceptor;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Properties;
+
+class HelloSocketInterceptor implements SocketInterceptor {
+    @Override
+    public void init(Properties properties) {
+        System.out.println("Initializing socket interceptor with properties");
+    }
+
+    @Override
+    public void onConnect(Socket connectedSocket) throws IOException {
+        System.out.println("Connected: " + connectedSocket.toString());
+    }
+}
