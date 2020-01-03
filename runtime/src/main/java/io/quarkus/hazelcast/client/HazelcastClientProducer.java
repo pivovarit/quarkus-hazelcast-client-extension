9154fa3ca45653df1ce481a357d8d79ba9c10fdf
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-03 13:13:55 +0200

    Accept 'yml' as config source (#59)

--- runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
+++ runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
@@ -66,6 +66,7 @@ public class HazelcastClientProducer {
         return source -> {
             switch (source) {
                 case "yaml":
+                case "yml":
                     return new ClientClasspathYamlConfig("hazelcast-client.yml");
                 case "xml":
                     return new ClientClasspathXmlConfig("hazelcast-client.xml");
