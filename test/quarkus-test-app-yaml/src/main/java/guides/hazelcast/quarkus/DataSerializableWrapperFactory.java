77adc6282c8c20dc0c88ac0fa3d1895b05358559
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-07 17:24:29 +0100

    Register DataSerializableFactoryExample

---
+++ test/quarkus-test-app-yaml/src/main/java/guides/hazelcast/quarkus/DataSerializableWrapperFactory.java
@@ -0,0 +1,15 @@
+package guides.hazelcast.quarkus;
+
+import com.hazelcast.nio.serialization.DataSerializableFactory;
+import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
+
+class DataSerializableWrapperFactory implements com.hazelcast.nio.serialization.DataSerializableFactory {
+    @Override
+    public IdentifiedDataSerializable create(int typeId) {
+        if (typeId == 42) {
+            return new DataSerializableWrapper();
+        }
+
+        throw new IllegalArgumentException(typeId + " unknown");
+    }
+}
