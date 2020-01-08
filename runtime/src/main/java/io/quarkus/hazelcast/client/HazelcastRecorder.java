7a1aa6c7c13c2726dc6c7437d8b5c7fe3d645889
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-08 21:05:02 +0100

    Add custom discovery strategy

--- runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastRecorder.java
+++ runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastRecorder.java
@@ -5,33 +5,22 @@ import io.quarkus.runtime.annotations.Recorder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
 
 @Recorder
 public class HazelcastRecorder {
     public void configureRuntimeProperties(HazelcastClientConfig config) {
         HazelcastClientProducer hazelcastClientProducer = Arc.container().instance(HazelcastClientProducer.class).get();
         hazelcastClientProducer.setHazelcastClientConfig(config);
-        hazelcastClientProducer.setYmlConfigPresent(existsNonempty("hazelcast-client.yml"));
-        hazelcastClientProducer.setYamlConfigPresent(existsNonempty("hazelcast-client.yaml"));
-        hazelcastClientProducer.setXmlConfigPresent(existsNonempty("hazelcast-client.xml"));
+        hazelcastClientProducer.setYmlConfigPresent(exists("hazelcast-client.yml"));
+        hazelcastClientProducer.setYamlConfigPresent(exists("hazelcast-client.yaml"));
+        hazelcastClientProducer.setXmlConfigPresent(exists("hazelcast-client.xml"));
     }
 
-    private boolean existsNonempty(String s) {
-        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(s);
-        if (stream == null) {
+    private boolean exists(String s) {
+        try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(s)) {
+            return stream != null;
+        } catch (IOException e) {
             return false;
-        }
-        try (Scanner scanner = new Scanner(stream, "UTF-8")) {
-            try {
-                return !scanner.useDelimiter("\\A").next().isEmpty();
-            } finally {
-                try {
-                    stream.close();
-                } catch (IOException e) {
-                    // silent close
-                }
-            }
         }
     }
 }
