2921d787775406596e3ba3c88bab77cc94d18b15
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-09 17:34:03 +0100

    Parse application.properties at build-time (#82)

--- runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
+++ runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
@@ -20,14 +20,13 @@ import java.util.stream.Stream;
 public class HazelcastClientProducer {
     private final AtomicReference<HazelcastInstance> instance = new AtomicReference<>(null);
 
-    private HazelcastClientConfig hazelcastClientConfig;
     private ClientConfig clientConfig;
 
     @Produces
     @Singleton
     @DefaultBean
     public ClientConfig hazelcastConfigClientInstance() {
-        return clientConfig != null ? clientConfig : fromApplicationProperties();
+        return clientConfig;
     }
 
     @Produces
@@ -39,83 +38,12 @@ public class HazelcastClientProducer {
         return instance;
     }
 
-    private ClientConfig fromApplicationProperties() {
-        ClientConfig clientConfig = new ClientConfig();
-
-        setClusterAddress(clientConfig);
-        setGroupName(clientConfig);
-        setLabels(clientConfig);
-
-        setOutboundPorts(clientConfig);
-        setOutboundPortDefinitions(clientConfig);
-
-        setConnectionTimeout(clientConfig);
-        setConnectionAttemptLimit(clientConfig);
-        setConnectionAttemptPeriod(clientConfig);
-
-        setExecutorPoolSize(clientConfig);
-
-        return clientConfig;
-    }
-
-    private void setExecutorPoolSize(ClientConfig clientConfig) {
-        hazelcastClientConfig.executorPoolSize
-          .ifPresent(clientConfig::setExecutorPoolSize);
-    }
-
-    private void setClusterAddress(ClientConfig clientConfig) {
-        clientConfig.getNetworkConfig().addAddress(hazelcastClientConfig.clusterMembers.split(","));
-    }
-
-    private void setGroupName(ClientConfig clientConfig) {
-        hazelcastClientConfig.groupName
-          .filter(s -> !s.isEmpty())
-          .ifPresent(groupName -> clientConfig.getGroupConfig().setName(groupName));
-    }
-
-    private void setLabels(ClientConfig clientConfig) {
-        hazelcastClientConfig.labels
-          .map(s -> Arrays.stream(s.split(","))).orElseGet(Stream::empty)
-          .forEach(clientConfig::addLabel);
-    }
-
-    private void setConnectionAttemptPeriod(ClientConfig clientConfig) {
-        hazelcastClientConfig.connectionAttemptPeriod
-          .ifPresent(period -> clientConfig.getNetworkConfig().setConnectionAttemptPeriod(period));
-    }
-
-    private void setConnectionAttemptLimit(ClientConfig clientConfig) {
-        hazelcastClientConfig.connectionAttemptLimit
-          .ifPresent(attempts -> clientConfig.getNetworkConfig().setConnectionAttemptLimit(attempts));
-    }
-
-    private void setConnectionTimeout(ClientConfig clientConfig) {
-        hazelcastClientConfig.connectionTimeout
-          .ifPresent(timeout -> clientConfig.getNetworkConfig().setConnectionTimeout(timeout));
-    }
-
-    private void setOutboundPortDefinitions(ClientConfig clientConfig) {
-        hazelcastClientConfig.outboundPortDefinitions
-          .map(str -> Arrays.stream(str.split(","))).orElseGet(Stream::empty)
-          .forEach(definition -> clientConfig.getNetworkConfig().addOutboundPortDefinition(definition));
-    }
-
-    private void setOutboundPorts(ClientConfig clientConfig) {
-        hazelcastClientConfig.outboundPorts
-          .map(str -> Arrays.stream(str.split(","))).orElseGet(Stream::empty)
-          .forEach(port -> clientConfig.getNetworkConfig().addOutboundPort(Integer.parseInt(port)));
-    }
-
     @PreDestroy
     public void destroy() {
         HazelcastInstance hazelcastInstance = instance.get();
         if (hazelcastInstance != null) {
             hazelcastInstance.shutdown();
         }
-    }
-
-    public void setHazelcastClientConfig(HazelcastClientConfig hazelcastClientConfig) {
-        this.hazelcastClientConfig = hazelcastClientConfig;
     }
 
     public void setClientConfig(ClientConfig clientConfig) {
