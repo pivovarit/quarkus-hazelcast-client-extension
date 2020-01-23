71e5c8a880be4f6fb6bf5edc9d50029b69a9ca10
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-23 12:58:43 +0100

    Shutdown all clients

--- runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
+++ runtime/src/main/java/io/quarkus/hazelcast/client/HazelcastClientProducer.java
@@ -1,6 +1,6 @@
 package io.quarkus.hazelcast.client;
 
-import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import io.quarkus.arc.DefaultBean;
 
@@ -10,32 +10,25 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 
 import static com.hazelcast.client.HazelcastClient.newHazelcastClient;
-import static java.util.Objects.requireNonNull;
 
 /**
  * @author Grzegorz Piwowarek
  */
 @ApplicationScoped
 public class HazelcastClientProducer {
-    private volatile HazelcastInstance instance = null;
-
     HazelcastClientConfig config;
 
     @Produces
     @Singleton
     @DefaultBean
     public HazelcastInstance hazelcastClientInstance() {
-        HazelcastInstance instance = newHazelcastClient(new HazelcastConfigurationResolver().resolveClientConfig(config));
-        this.instance = instance;
-        return instance;
+        return newHazelcastClient(new HazelcastConfigurationResolver()
+          .resolveClientConfig(config));
     }
 
     @PreDestroy
     public void destroy() {
-        HazelcastInstance hazelcastInstance = instance;
-        if (hazelcastInstance != null) {
-            hazelcastInstance.shutdown();
-        }
+        HazelcastClient.shutdownAll();
     }
 
     public void injectConfig(HazelcastClientConfig config) {
